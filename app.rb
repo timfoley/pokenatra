5c7af500e69023f49c5842a183fa5c98738de18c
Tim Foley <thetimfoley@gmail.com>
2016-10-05 13:50:44 -0400

    added a few views

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+# Load the file to connect to the DB
+require_relative 'db/connection'
+
+# Load models
+require_relative 'models/pokemon'
+require_relative 'models/trainer'
+
+get '/' do
+  haml :index
+end
+
+get '/pokemon' do
+  @pokemons = Pokemon.all
+  haml :'pokemon/index'
+end
+
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  haml :'pokemon/show'
+end
